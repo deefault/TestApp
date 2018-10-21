701be27119f68b87902e5f4d0deae987342e68fc
Alexander Solovyov <alextomsky@protonmail.com>
2018-10-21 13:58:07 +0500

    1

---
+++ WebApplication3/Models/UserModel.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication3.Models
+{
+    public class User : IdentityUser
+    {
+        
+    }
+}

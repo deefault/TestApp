d697d0e75d84d5993908f991c0fc148a0ab714f1
Alexander Solovyov <alextomsky@protonmail.com>
2018-10-31 23:40:11 +0500

    Removing all files in .gitignore

---
+++ WebApplication3/Data/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication3.Models;
+using System.Linq;
+
+namespace WebApplication3.Data
+{
+    class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+           
+            if (context.Users.Any())
+            {
+                return;
+            }
+
+            var users = new User[]
+            {
+                new User{UserName = "user1",Email = "example@sasd.com"},
+                new User{UserName = "user2",Email = "example2@sasd.com"},
+                
+            };
+            
+            foreach (var user in users)
+            {
+                context.Users.Add(user);
+            }
+
+            context.SaveChanges();
+
+        }
+    }
+}

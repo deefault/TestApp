1f12b62ec713ad53245ae7d35a1168bc3c04e1c5
Alexander Solovyov <alextomsky@protonmail.com>
2018-11-05 16:48:31 +0500

    added test details

--- WebApplication3/Controllers/TestController.cs
+++ WebApplication3/Controllers/TestController.cs
@@ -8,11 +8,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.HPack;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using WebApplication3.Data;
 using WebApplication3.Models;
 using WebApplication3.Models.TestViewModels;
+using StatusCodes = Microsoft.AspNetCore.Http.StatusCodes;
 
 namespace WebApplication3.Controllers
 {
@@ -90,8 +92,77 @@ namespace WebApplication3.Controllers
         [Route("/[controller]s/{id}/")]
         public async Task<IActionResult> Details(int id)
         {
-            var test = _context.Tests.SingleAsync(t => t.Id == id);
-            throw new NotImplementedException();
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var test = await _context.Tests.SingleAsync(t => t.Id == id);
+            if (test == null)
+            {
+                Response.StatusCode = 404;
+                return View();
+            }
+            ViewData["user"] = user;
+            if (test.CreatedBy == user)
+            {
+                
+                return View(test);
+            }
+            else
+            {
+                var testResult = _context.TestResults.Where(r => r.Test == test || r.CompletedByUser == user).FirstAsync();
+                // у пользователя отсутствует тест
+                if (testResult == null)
+                {
+                    // добавляем тест к пользователю
+                    return RedirectToAction("AddTestToUser",new {testId = test.Id, userId=user.Id});
+                }
+
+                ViewData["testResult"] = testResult;
+                return View(test);
+            }
+            return View(test);
+
+            
+
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("User/[controller]s/{testId}/AddTestToUser/")]
+        public async Task<IActionResult> AddTestToUser(int testId)
+        {
+            //throw new NotImplementedException();
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var test = await _context.Tests.SingleAsync(t => t.Id == testId);
+            if (test == null)
+            {
+                return NotFound();
+            }
+            TestResult testResult = new TestResult
+            {
+                isCompleted = false, 
+                Test = test,
+                CompletedByUser = user,
+                CompletedOn = DateTime.Now
+            };
+            await _context.TestResults.AddAsync(testResult);
+            await _context.SaveChangesAsync();
+            Response.StatusCode = 200;
+            return RedirectToAction("Details", new {id = testId});
+        }
+        
+        [HttpPost]
+        [Authorize]
+        [Route("User/[controller]s/{testId}/AddTestToUser/")]
+        public async Task<IActionResult> AddTestToUser(AddTestToUserViewModel model,int testId)
+        {
+            var test = await _context.Tests.SingleAsync(t => t.Id == testId);
+            if (test == null)
+            {
+                Response.StatusCode = 404;
+                return NotFound();
+            }
+
+            ViewData["test"] = test;
+            return View(model);
         }
     }
 }

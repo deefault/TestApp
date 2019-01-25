ec6ed5c06f10efc2b0625dae882cd96f9d4bf723
Alexander Solovyov <wsssewssse3@gmail.com>
2019-01-25 20:37:24 +0500

    move scripts to site.js

--- WebApplication3/Controllers/QuestionController.cs
+++ WebApplication3/Controllers/QuestionController.cs
@@ -64,7 +64,7 @@ namespace WebApplication3.Controllers
                 case (int) Question.QuestionTypeEnum.TextQuestion:
                     return View("AddTextQuestion");
                 case (int)Question.QuestionTypeEnum.DragAndDropQuestion:
-                    return View("AddDragAndDropQuestion");
+                    return View("AddDnDQuestion");
                 default:
                     return View("AddSingleChoiceQuestion");
             }

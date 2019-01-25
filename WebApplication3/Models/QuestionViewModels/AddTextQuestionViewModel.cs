6574c263ca229649c9409567820b474e71c72f5d
Alexey Kochkin <kochkin@protonmail.com>
2019-01-26 00:53:16 +0500

    Details TextQuestion (костыль)

--- WebApplication3/Models/QuestionViewModels/AddTextQuestionViewModel.cs
+++ WebApplication3/Models/QuestionViewModels/AddTextQuestionViewModel.cs
@@ -8,7 +8,24 @@ namespace WebApplication3.Models.QuestionViewModels
 {
     public class AddTextQuestionViewModel : QuestionViewModel
     {
+        public class TextViewModel
+        {
+            [Required]
+            public string Text { get; set; }
+        }
         [Required]
         public string QuestionType { get; set; }
+        
+        [OptionsValidation]
+        public List<TextViewModel> Options { get; set; }
+
+        public class OptionsValidationAttribute : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                var options = value as List<TextViewModel>;
+                return ValidationResult.Success;
+            }
+        }
     }
 }

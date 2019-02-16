476ddd5fab349a812b6c558e8877995d4f5fdf0a
Alexander Solovyov <wsssewssse3@gmail.com>
2019-02-16 14:15:32 +0500

    SingleChoiceAnswer add

---
+++ WebApplication3/Models/AnswerViewModels/SingleChoiceAnswerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models.AnswerViewModels
+{
+    public class SingleChoiceAnswerViewModel
+    {
+        [Required]
+        public int OptionId { get; set; }
+    }
+}

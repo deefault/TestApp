c4898f94ba6033e4343ba40db1e9de6fbc4bbbf3
Alexey Kochkin <kochkin@protonmail.com>
2019-03-03 23:38:37 +0500

    Токены с указанием строки

--- WebApplication3/TParser/Parser.cs
+++ WebApplication3/TParser/Parser.cs
@@ -9,7 +9,7 @@ namespace WebApplication3.TParser
     class Parser
     {
         private static Hashtable _types;
-        public static TestData Parse(Queue<string> tokens)
+        public static TestData Parse(Queue<Token> tokens)
         {
             _types = Hashtable.Synchronized(new Hashtable());
             _types["single"] = 1;
@@ -26,56 +26,57 @@ namespace WebApplication3.TParser
                 throw new Exception(e.Message);
             }
         }
-        private static string Consume(Queue<string> tokens, string desired)
+        private static string Consume(Queue<Token> tokens, string desired)
         {
-            string token = tokens.Peek().ToLower();
+            string token = tokens.Peek().Value.ToLower();
             if (token == desired)
             {
-                return tokens.Dequeue();
+                return tokens.Dequeue().Value;
             }
-            throw new Exception(String.Format("Expected: {0}; Found: {1}", desired, token));
+            throw new Exception(String.Format("Expected: {0}; Found: {1}; Row - {2}", desired, token, tokens.Peek().Row));
 
         }
-        private static int ConsumeType(Queue<string> tokens)
+        private static int ConsumeType(Queue<Token> tokens)
         {
-            int? token = (int?)_types[tokens.Peek().ToLower()];
+            int? token = (int?)_types[tokens.Peek().Value.ToLower()];
             if (token != null)
             {
                 tokens.Dequeue();
                 return (int)token;
             }
-            throw new Exception(String.Format("Expected: {0}; Found: {1}", "QuestionType", tokens.Peek()));
+            throw new Exception(String.Format("Expected: {0}; Found: {1}; Row - {2}", "QuestionType", tokens.Peek().Value, tokens.Peek().Row));
 
         }
-        private static bool ConsumeFlag(Queue<string> tokens)
+        private static bool ConsumeFlag(Queue<Token> tokens)
         {
-            bool flag = Boolean.TryParse(tokens.Peek(), out bool tmp);
+            bool flag = Boolean.TryParse(tokens.Peek().Value, out bool tmp);
             if (flag)
             {
                 tokens.Dequeue();
                 return tmp;
             }
-            throw new Exception(String.Format("Expected: {0}; Found: {1}", "Boolean", tokens.Peek()));
+            throw new Exception(String.Format("Expected: {0}; Found: {1}; Row - {2}", "Boolean", tokens.Peek().Value, tokens.Peek().Row));
         }
-        private static int ConsumeScore(Queue<string> tokens)
+        private static int ConsumeScore(Queue<Token> tokens)
         {
-            bool flag = Int32.TryParse(tokens.Peek(), out int tmp);
+            bool flag = Int32.TryParse(tokens.Peek().Value, out int tmp);
             if (flag)
             {
                 tokens.Dequeue();
                 return tmp;
             }
-            throw new Exception(String.Format("Expected: {0}; Found: {1}", "Int32", tokens.Peek()));
+            throw new Exception(String.Format("Expected: {0}; Found: {1}; Row - {2}", "Int32", tokens.Peek().Value, tokens.Peek().Row));
         }
-        private static TestData ParseTest(Queue<string> tokens)
+        private static TestData ParseTest(Queue<Token> tokens)
         {
             TestData testData = new TestData();
             var test = new Test();
             bool textParsed = false, flagParsed = false;
+            int Row = tokens.Peek().Row;
             Consume(tokens, "test");
             Consume(tokens, "{");
-            while (tokens.Peek() != "}") 
-                switch (tokens.Peek())
+            while (tokens.Peek().Value != "}") 
+                switch (tokens.Peek().Value)
                 {
                     case ("text"):
                         if (!textParsed)
@@ -97,23 +98,24 @@ namespace WebApplication3.TParser
                 }
             //test.Name = ParseText(tokens);
             //test.IsEnabled = ParseFlag(tokens);
-            //while (tokens.Peek() == "question")
+            //while (tokens.Peek().Value == "question")
             //{
             //    ParseQuestion(tokens, test, testData);
             //}
             Consume(tokens, "}");
             testData.Test = test;
             if (!textParsed || !flagParsed)
-                throw new Exception("Заданы не все требуемые поля (Test)");
+                throw new Exception(String.Format("Заданы не все требуемые поля (Test (Row - {0})). Text - {1}, Flag - {2}.", Row, textParsed, flagParsed));
             return testData;
         }
-        private static void ParseQuestion(Queue<string> tokens, Test test, TestData testData)
+        private static void ParseQuestion(Queue<Token> tokens, Test test, TestData testData)
         {
             Question question;
+            int i = 1, checkedCount = 0, Row = 0;
             bool textParsed = false, scoreParsed = false, optionParsed = false;
+            Row = tokens.Peek().Row;
             Consume(tokens, "question");
             Consume(tokens, "{");
-            int i = 1, checkedCount = 0;
             var type = ParseType(tokens);
             switch (type)
             {
@@ -134,21 +136,21 @@ namespace WebApplication3.TParser
                     break;
             }
             question.Test = test;
-            while (tokens.Peek() != "}")
-                switch (tokens.Peek())
+            while (tokens.Peek().Value != "}")
+                switch (tokens.Peek().Value)
                 {
                     case ("text"):
                         if (!textParsed)
                             question.Title = ParseText(tokens);
                         else
-                            throw new Exception("Text already parsed");
+                            throw new Exception(String.Format("Text already parsed (Quesiton (Row - {0})).", Row));
                         textParsed = true;
                         break;
                     case ("score"):
                         if (!scoreParsed)
                             question.Score = ParseScore(tokens);
                         else
-                            throw new Exception("Score already parsed");
+                            throw new Exception(String.Format("Score already parsed (Quesiton (Row - {0})).", Row));
                         scoreParsed = true;
                         break;
                     case ("option"):
@@ -156,40 +158,25 @@ namespace WebApplication3.TParser
                             if (i == 1)
                                 ParseOption(tokens, question, testData, i++, ref checkedCount);
                             else
-                                throw new Exception("TextQuestion поддерживает только один опшн");
+                                throw new Exception(String.Format("TextQuestion поддерживает только один опшн (Quesiton (Row - {0})).", Row));
                         else
                             ParseOption(tokens, question, testData, i++, ref checkedCount);
                         optionParsed = true;
                         break;
                 }
             question.QuestionType = Enum.GetName(typeof(Question.QuestionTypeEnum), type);
-            //if (question is TextQuestion)
-            //    ParseOption(tokens, question, testData, i++, ref checkedCount);
-            //else
-            //{
-            //    while (tokens.Peek() == "option")
-            //    {
-            //        ParseOption(tokens, question, testData, i++, ref checkedCount);
-            //    }
-            //    if (question is SingleChoiceQuestion && checkedCount != 1)
-            //    {
-            //            throw new Exception("В данном типе вопроса нужно отметить один ответ.");
-            //    }
-            //    else if (question is MultiChoiceQuestion && checkedCount < 1)
-            //        throw new Exception("В данном типе вопроса нужно отметить хотя бы один ответ.");
-            //}
             if (question is SingleChoiceQuestion && checkedCount != 1)
             {
-                throw new Exception("В данном типе вопроса нужно отметить один ответ.");
+                throw new Exception(String.Format("В данном типе вопроса нужно отметить один ответ (Quesiton (Row - {0})).", Row));
             }
             else if (question is MultiChoiceQuestion && checkedCount < 1)
-                throw new Exception("В данном типе вопроса нужно отметить хотя бы один ответ.");
+                throw new Exception(String.Format("В данном типе вопроса нужно отметить хотя бы один ответ (Quesiton (Row - {0})).", Row));
             Consume(tokens, "}");
             if (!textParsed || !scoreParsed || !optionParsed)
-                throw new Exception("Заданы не все требуемые поля (Quesiton)");
+                throw new Exception(String.Format("Заданы не все требуемые поля (Quesiton (Row - {0})). Text - {1}, Score - {2}, Option - {3}.", Row, textParsed, scoreParsed, optionParsed));
             testData.Questions.Add(question);
         }
-        private static void ParseOption(Queue<string> tokens, Question question, TestData testData, int i, ref int checkedCount)
+        private static void ParseOption(Queue<Token> tokens, Question question, TestData testData, int i, ref int checkedCount)
         {
             Option option = new Option { Question = question };
             Consume(tokens, "option");
@@ -208,28 +195,28 @@ namespace WebApplication3.TParser
             Consume(tokens, "}");
             testData.Options.Add(option);
         }
-        private static string ParseText(Queue<string> tokens)
+        private static string ParseText(Queue<Token> tokens)
         {
             Consume(tokens, "text");
             Consume(tokens, "=");
-            string text = tokens.Dequeue();
+            string text = tokens.Dequeue().Value;
             return text;
         }
-        private static bool ParseFlag(Queue<string> tokens)
+        private static bool ParseFlag(Queue<Token> tokens)
         {
             Consume(tokens, "flag");
             Consume(tokens, "=");
             bool flag = ConsumeFlag(tokens);
             return flag;
         }
-        private static int ParseScore(Queue<string> tokens)
+        private static int ParseScore(Queue<Token> tokens)
         {
             Consume(tokens, "score");
             Consume(tokens, "=");
             int score = ConsumeScore(tokens);
             return score;
         }
-        private static int ParseType(Queue<string> tokens)
+        private static int ParseType(Queue<Token> tokens)
         {
             Consume(tokens, "type");
             Consume(tokens, "=");

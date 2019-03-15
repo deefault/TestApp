2fa6298d7f62ce72d4846305251e167d7211a843
Alexey Kochkin <kochkin@protonmail.com>
2019-03-15 22:59:43 +0500

    Включение/выключение теста

--- WebApplication3/Models/QuestionViewModels/QuestionViewModel.cs
+++ WebApplication3/Models/QuestionViewModels/QuestionViewModel.cs
@@ -6,17 +6,20 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication3.Models.QuestionViewModels
-{    
-    public class QuestionViewModel 
+{
+    public class QuestionViewModel
     {
         public string Text { get; set; }
 
         [Required]
+        [DisplayName("Вопрос")]
         public string Title { get; set; }
 
         [Required]
         public int TestId { get; set; }
+
         [Range(1, 100)]
+        [DisplayName("Балл")]
         public int Score { get; set; } = 1;
     }
 }

f5152c8a1773dfb124289a0442cf57301272a1be
Alexander Solovyov <wsssewssse3@gmail.com>
2019-03-17 22:29:21 +0500

    добавление теста по QR коду

--- WebApplication3/Models/QuestionViewModels/QuestionViewModel.cs
+++ WebApplication3/Models/QuestionViewModels/QuestionViewModel.cs
@@ -12,11 +12,14 @@ namespace WebApplication3.Models.QuestionViewModels
         public string Text { get; set; }
 
         [Required]
+        [DataType(DataType.MultilineText)]
+        [DisplayName("Формулировка вопроса")]
         public string Title { get; set; }
 
         [Required]
         public int TestId { get; set; }
         [Range(1, 100)]
+        [DisplayName("Балл")]
         public int Score { get; set; } = 1;
     }
 }
--- WebApplication3/Utils/Utils.cs
+++ WebApplication3/Utils/Utils.cs
@@ -4,6 +4,7 @@ namespace WebApplication3.Utils
 {
     public class Utils
     {
+        
         public static Bitmap GenerateQRCodeFromLink(string link)
         {
             
@@ -11,6 +12,16 @@ namespace WebApplication3.Utils
             QRCodeData qrCodeData  = qrGenerator.CreateQrCode(link, QRCodeGenerator.ECCLevel.Q);
             QRCode qrCode = new QRCode(qrCodeData);
             Bitmap image = qrCode.GetGraphic(20, Color.Brown, Color.White, false);
+            return image;
+        }
+        
+        public static string GenerateBase64QRCodeFromLink(string link)
+        {
+            
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData  = qrGenerator.CreateQrCode(link, QRCodeGenerator.ECCLevel.Q);
+            Base64QRCode qrCode = new Base64QRCode(qrCodeData);
+            string image = qrCode.GetGraphic(50);
             return image;
         }
     }
